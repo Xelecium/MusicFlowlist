6b0aa15280a909f9d2fbcec07d016cd75588c46d
Xelecium <suiken.hao+github@gmail.com>
2015-06-08 15:40:43 -0700

    Added in database schema, with column definitions and statements implemented

---
+++ app/src/main/java/xeleciumlabs/musicflowlist/data/TrackListSQLiteHelper.java
@@ -0,0 +1,54 @@
+package xeleciumlabs.musicflowlist.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Xelecium on 6/8/2015.
+ */
+public class TrackListSQLiteHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "tracks.db";
+    private static final int DB_VERSION = 1;
+
+    //Table of leading tracks
+    public static final String LEAD_TABLE = "LEAD_TRACKS";  //Table name
+    public static final String COLUMN_LEAD_URI = "LEAD_URI";  //Track's URI
+
+    private static String CREATE_LEAD_TRACKS =
+            "CREATE TABLE " + LEAD_TABLE +                      //Create Table w/ name
+            "(" + COLUMN_LEAD_URI + " TEXT PRIMARY KEY)";       //Track's URI is primary key
+
+    //Table of following tracks
+    public static final String FOLLOW_TABLE = "FOLLOW_TRACKS";  //Table name
+    public static final String COLUMN_FOREIGN_KEY_LEAD = "LEAD_URI";    //Leading Track's URI
+    public static final String COLUMN_FOLLOW_URI = "FOLLOW_URI";    //Follow Track's URI
+
+    private static String CREATE_FOLLOW_TRACKS =
+            "CREATE TABLE " + FOLLOW_TABLE +                                //Create Table w/ name
+            "(" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + //Auto-incremented ID# as PKEY
+            COLUMN_FOREIGN_KEY_LEAD + " TEXT, " +                           //Leading Track's URI
+            COLUMN_FOLLOW_URI + " TEXT, " +                                 //Follow Track's URI
+            "FOREIGN KEY(" + COLUMN_FOREIGN_KEY_LEAD + ") " +               //Specify LeadTrack as FKEY
+            "REFERENCES " + LEAD_TABLE + "(" + COLUMN_LEAD_URI + "))";      //Referencing LeadTable
+
+    //Constructor for the database
+    public TrackListSQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CREATE_LEAD_TRACKS);
+        database.execSQL(CREATE_FOLLOW_TRACKS);
+    }
+
+    @Override   //Method for updating database schema between versions
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        switch (oldVersion) {
+            default: break;
+        }
+    }
+}
