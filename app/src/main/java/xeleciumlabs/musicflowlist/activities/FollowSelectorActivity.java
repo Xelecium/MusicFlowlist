ef934e7b408b68f0fc14f1e080829b744664bb7f
Xelecium <suiken.hao+github@gmail.com>
2015-06-11 16:02:32 -0700

    Sets current track's album art
    FollowSelectorActivity now displays the current track's album art. Still
    need to update the loading of the bitmap as an asynchronous task.

--- app/src/main/java/xeleciumlabs/musicflowlist/activities/FollowSelectorActivity.java
+++ app/src/main/java/xeleciumlabs/musicflowlist/activities/FollowSelectorActivity.java
@@ -6,14 +6,19 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import xeleciumlabs.musicflowlist.MusicService;
@@ -98,6 +103,27 @@ public class FollowSelectorActivity extends Activity {
         FollowTrackAdapter adapter = new FollowTrackAdapter(this, mTracks);
         mTrackListView.setAdapter(adapter);
 
+        //Setting up the View for the currently selected track
+        mCurrentTrackAlbumArt = (ImageView) findViewById(R.id.albumArt);
+        mCurrentTrackTitle = (TextView)findViewById(R.id.track_title);
+
+        Track currentTrack = mTracks.get(mSelectedTrack);
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), currentTrack.getAlbumArt());
+        } catch (FileNotFoundException exception) {
+            //If a track doesn't have an associated album art, we'll provide a default one
+            exception.printStackTrace();
+            bitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.empty_albumart);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        mCurrentTrackAlbumArt.setImageBitmap(bitmap);
+        mCurrentTrackTitle.setText(currentTrack.getTitle());
         //Not setting an empty view because we can't get to this activity with an empty list,
         // as there's nothing to click
 
